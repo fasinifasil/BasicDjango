18b96f347d5a421ba09fe6c2e0eab6cf871888ec
fasini fasil <fasinifasil@gmail.com>
2024-05-30 03:14:09 +0530

    movie

--- MovieApp/admin.py
+++ MovieApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from MovieApp.models import MovieDb, Director,CensorInfo
+from MovieApp.models import MovieDb, Director,CensorInfo,Actor
 
 # Register your models here.
 admin.site.register(MovieDb)
 admin.site.register(Director)
 admin.site.register(CensorInfo)
+admin.site.register(Actor)
--- MovieApp/models.py
+++ MovieApp/models.py
@@ -10,17 +10,25 @@ class CensorInfo(models.Model):
     def __str__(self):
         return self.certifedBy
 
+class Actor(models.Model):
+    actorName   =   models.CharField(max_length=50)
+    def __str__(self):
+        return self.actorName
+
+
+class Director(models.Model):
+    DirectorName    =   models.CharField(max_length=40)
+    def __str__(self):
+        return self.DirectorName
 class MovieDb(models.Model):
     Movie_Title     =   models.CharField(max_length=30)
     ReleasedYear    =   models.IntegerField(null=True)
     Description     =   models.TextField()
     MoviePic        =   models.ImageField(upload_to='poster',null=True)
+    cast            =   models.ManyToManyField(Actor,related_name='moviecast')
+    movieDirector   =   models.ForeignKey(Director,on_delete=models.SET_NULL,null=True,related_name='directedBy')
     censoredBy      =   models.OneToOneField(CensorInfo,on_delete=models.CASCADE,related_name='movie',null=True)
     def __str__(self):
         return self.Movie_Title
-class Director(models.Model):
-    DirectorName    =   models.CharField(max_length=40)
-    def __str__(self):
-        return self.DirectorName
 
 
